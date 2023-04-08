ce2948321e41043cbfb2b267dd38fe52bce4af09
System Administrator <root@liaoqianyudeMacBook-Pro.local>
2023-04-08 20:53:44 +0800

    首次提交测试分支

---
+++ src/main/java/org/dhkj/trans/TransApplication.java
@@ -0,0 +1,16 @@
+package org.dhkj.trans;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransApplication {
+    private static final Logger logger = LoggerFactory.getLogger(TransApplication.class);
+    public static void main(String[] args) {
+        logger.error("我是第一次启动");
+        SpringApplication.run(TransApplication.class, args);
+    }
+
+}
